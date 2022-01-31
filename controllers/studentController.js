e07e8af45ad03810fe545f6e74e72dc17f75b905
FlabioM <mflabio@yahoo.com>
2022-01-31 08:52:53 -0800

    initial commit

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,5 +1,4 @@
-const {Student} = require('../models')
-
+const {Student, Course} = require('../models')
 
 module.exports.viewAll = async function(req, res){
     const students = await Student.findAll();
@@ -8,6 +7,33 @@ module.exports.viewAll = async function(req, res){
 
 
 module.exports.viewProfile= async function(req,res){
-    const student = await Student.findByPk(req.params.id);
-    res.render('student/profile', {student})
+    const student = await Student.findByPk(req.params.id, {
+        include: 'courses'
+    });
+    const courses = await Course.findAll();
+    let availableCourses = [];
+    for (let i=0; i<courses.length; i++){
+        if (!studentHasCourse(student, courses[i])){
+            availableCourses.push(courses[i]);
+        }
+    }
+    res.render('student/profile', {student, availableCourses})
 }
+
+module.exports.addStudent = async function(req, res){
+    const student = await Student.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        grade_level: req.body.grade_level
+    });
+    res.redirect(`/students/profile/${student.id`);
+};
+
+function studentHasCourse(student, course){
+    for (let i=0; i<student.courses.length; i++){
+        if (course.id === student.courses[i].id){
+            return true
+        }
+    }
+    return false
+};
